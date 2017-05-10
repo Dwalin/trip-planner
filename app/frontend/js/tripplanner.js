c917ce34e411b2ba3059bd40fc7e3babf739cc58
Oles <ibolosig@gmail.com>
2017-05-10 16:55:40 +0300

    Direction maps.

--- app/frontend/js/tripplanner.js
+++ app/frontend/js/tripplanner.js
@@ -390,9 +390,8 @@ $(function() {
 		init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
 
 			console.log("------------------------------");
-			console.log(bindingContext);
-			console.log(bindingContext.$parent);
-			//console.log(bindingContext.$parent.location());
+			console.log(bindingContext.$data.location());
+			console.log(bindingContext.$parent.stops()[bindingContext.$index-1].location());
 			console.log("------------------------------");
 
 
