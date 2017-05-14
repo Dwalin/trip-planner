d7807d7c00ffd5f546c56477025164e04b965e46
Oles <ibolosig@gmail.com>
2017-05-14 16:32:08 +0300

    Trying to position leaflet map on "from" position.

--- dist/js/tripplanner.js
+++ dist/js/tripplanner.js
@@ -446,6 +446,7 @@ $(function() {
 								//).addTo(map);
 
 								L.Routing.control({
+									show: false,
 									waypoints: [
 										L.latLng(57.74, 11.94),
 										L.latLng(57.6792, 11.949)
