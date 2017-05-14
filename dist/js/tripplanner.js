b141f9e9c19729c4fd9a590fa7115a56cabb1fd2
Oles <ibolosig@gmail.com>
2017-05-14 15:22:00 +0300

    Trying to position leaflet map on "from" position.

--- dist/js/tripplanner.js
+++ dist/js/tripplanner.js
@@ -424,7 +424,7 @@ $(function() {
 								coordFrom = [resultFrom[0].y, resultFrom[0].x];
 								coordTo   = [resultTo[0].y,   resultTo[0].x];
 
-								var map = L.map(element).setView(coord, 8);
+								var map = L.map(element).fitBounds(coordFrom, coordTo);
 
 								L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoib2tyeXpoYW5pdnNreWkiLCJhIjoiY2oyb2xhcHA0MDAyOTJxcGZrdHQ4ZG0xZyJ9.7h-IQAfbm-AxbXAhEo5grw', {
 									attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
